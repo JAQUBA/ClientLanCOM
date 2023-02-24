1b58585a83a6e93b7e9bef068f890e6e15ec6ff9
Karol Jakubowski <kjakubowski0492@gmail.com>
2023-02-24 14:54:28 +0100

    Serial0+1 ->Atmega328

--- ClientLanCOM.ino
+++ ClientLanCOM.ino
@@ -1,107 +1,129 @@
-#include <SPI.h>
-#include <UIPEthernet.h>
+#define UIP_CONF_UDP 0
+#define UIP_CONF_MAX_CONNECTIONS 1
+
+#include <EthernetENC.h>
 #include <avr/wdt.h>
 
 #define STATIC
-
-byte MACADDRESS[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-
-#define UARTBAUD 9600
-
+#define ETH_RESET 4
+byte MACADDRESS[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE};
 #ifdef STATIC
-  #define MYIPADDR IPAddress(192,168,0,101)
+  #define MYIPADDR IPAddress(192,168,0,231)
   #define MYIPMASK IPAddress(255,255,255,0)
   #define MYDNS IPAddress(192,168,0,1)
   #define MYGW IPAddress(192,168,0,1)
 #endif
 
-#define LED 8
-#define SERIAL_TIMEOUT 5
-
-IPAddress server(192,168,0,102);
-#define PORT 5556
+IPAddress host(192,168,0,102);
+#define PORT 5557
 
 EthernetClient client;
+EthernetServerPrint server = EthernetServerPrint(1000);
+
+#define LED 7
+unsigned long previousLED = 0;
+unsigned long interruptionLED = 500;
+bool statusLED = false;
+
+#define SERIAL0_BAUD 9600
+#define SERIAL0_TIMEOUT 6
+#define SERIAL0_BUF_SIZE 255
+unsigned long serial0Timestamp = 0;
+char serial0Buffer[SERIAL0_BUF_SIZE];
+unsigned int serial0BufferNum = 0;
+
+#define SERIAL1_BAUD 9600
+#define SERIAL1_TIMEOUT 6
+#define SERIAL1_BUF_SIZE 255
+unsigned long serial1Timestamp = 0;
+char serial1Buffer[SERIAL1_BUF_SIZE];
+unsigned int serial1BufferNum = 0;
 
 void setup() {
-  Serial.begin(UARTBAUD);
+  Serial.begin(SERIAL0_BAUD);
+  Serial1.begin(SERIAL1_BAUD);
   
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
+  pinMode(ETH_RESET, OUTPUT);
+  digitalWrite(ETH_RESET, LOW);
   delay(200);
-  digitalWrite(4, HIGH);
-  delay(2000);
+  digitalWrite(ETH_RESET, HIGH);
+  delay(200);
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, statusLED=!statusLED);
   
 #ifdef STATIC
   Ethernet.begin(MACADDRESS, MYIPADDR, MYDNS, MYGW, MYIPMASK);
 #else
   Ethernet.begin(MACADDRESS);
 #endif
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
 
+  server.begin();
   wdt_enable(WDTO_8S);
-}
-
-void connect() {
-  client.stop();
-  if (!client.connect(server, PORT)) {
-    delay(1000);
-  }
 }
 
 unsigned long previousPing = 0;
 unsigned long interruptionPing = 4000;
-
-unsigned long previousLED = 0;
-unsigned long interruptionLED = 500;
-bool statusLED = false;
-
-unsigned long previousSerial = 0;
-
-char serialBuffer[256];
-unsigned int serialBufferNum = 0;
 
 void loop() {
   wdt_reset();
   Ethernet.maintain();
   unsigned long currentMillis = millis();
 
-
   if(previousLED+interruptionLED < currentMillis) {
     previousLED = currentMillis;
-    digitalWrite(LED, statusLED=!statusLED);
+  }
+  digitalWrite(LED, statusLED=!statusLED);
+  
+  size_t size;
+  EthernetClient sClient = server.available();
+  if(sClient) {
+    while((size = sClient.available()) > 0) {
+      uint8_t* msg = (uint8_t*)malloc(size+1);
+      memset(msg, 0, size+1);
+      size = sClient.read(msg, size);
+      Serial1.write(msg, size);
+      free(msg);
+    }
+  }
+ while(Serial1.available() > 0) {
+    serial1Timestamp = currentMillis;
+    serial1Buffer[serial1BufferNum] = (char)Serial1.read();
+    if(serial1BufferNum++ >= SERIAL1_BUF_SIZE-1) {
+      serial1Timestamp = 0;
+      break;
+    }
+  }
+  if(serial1BufferNum > 0 && (serial1Timestamp+SERIAL1_TIMEOUT <= currentMillis)) {
+    server.write(serial1Buffer, serial1BufferNum);
+    serial1BufferNum = 0;
   }
   
   if(previousPing+interruptionPing < currentMillis) {
     previousPing = currentMillis;
+    if(!client.connected() && !client.connect(host, PORT)) return;
     client.write(0xFF);
   }
-  
-  if (!client.connected()) {
-    connect();
-    return;
+
+  while((size = client.available()) > 0) {
+    uint8_t* msg = (uint8_t*)malloc(size+1);
+    memset(msg, 0, size+1);
+    size = client.read(msg, size);
+    Serial.write(msg, size);
+    free(msg);
+    previousPing = currentMillis;
   }
   
-  if(Serial.available() > 0) {
-    previousSerial = currentMillis;
-
-    while(Serial.available() > 0) {
-      char recv = (char)Serial.read();
-      serialBuffer[serialBufferNum] = recv;
-      serialBufferNum++;
+  while(Serial.available() > 0) {
+    serial0Timestamp = currentMillis;
+    serial0Buffer[serial0BufferNum] = (char)Serial.read();
+    if(serial0BufferNum++ >= SERIAL0_BUF_SIZE-1) {
+      serial0Timestamp = 0;
+      break;
     }
   }
-
-  if((previousSerial+SERIAL_TIMEOUT < currentMillis) && serialBufferNum > 0) {
-    client.flush();
-    client.write(serialBuffer, serialBufferNum);
-    serialBufferNum = 0;
-  }
-
-  while(client.available() > 0) {
-    char recv = (char)client.read();
-    Serial.print(recv);
-    previousPing = currentMillis;
+  if(serial0BufferNum > 0 && (serial0Timestamp+SERIAL0_TIMEOUT < currentMillis)) {
+    client.write(serial0Buffer, serial0BufferNum);
+    serial0BufferNum = 0;
   }
 }
